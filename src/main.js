ab91569756723e8327174bafca3ddd27ab7301f0
CaiJinyc <me@caijin.tech>
2018-05-09 17:25:47 +0800

    init vue-cli

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})

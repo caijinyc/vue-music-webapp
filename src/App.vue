dae8b0445f34fc0c599e049e5aa78c5a91db1bfe
CaiJinyc <me@caijin.tech>
2018-05-15 20:22:38 +0800

    解决移动端不能自动播放问题

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,32 @@ import Tab from 'cpnts/tab/tab'
 import Player from 'cpnts/player/player'
 
 export default {
+  // 这里的代码除了 components，其他的都是用来解决 移动端需不能自动播放的问题
+  // 方法很鬼畜，看看就好，找到好的解决办法我会改的
+  data () {
+    return {
+      stop: false
+    }
+  },
+  mounted () {
+    let m = document.querySelector('#app')
+    m.addEventListener('touchend', this.firstPlay)
+  },
+  methods: {
+    firstPlay () {
+      let music = document.querySelector('#music-audio')
+      music.play()
+      if (music.src !== '') {
+        this.stop = true
+      }
+    }
+  },
+  watch: {
+    stop () {
+      let m = document.querySelector('#app')
+      m.removeEventListener('touchend', this.firstPlay)
+    }
+  },
   components: {
     MHeader,
     Tab,
